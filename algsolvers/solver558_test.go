f6995023b584fe68bcf33a698d48fb485bb2cda7
KitlerUA <volodymyr.kit.ua@gmail.com>
2017-07-09 11:44:02 +0300

    Fix wrong test in Solver558

--- algsolvers/solver558_test.go
+++ algsolvers/solver558_test.go
@@ -5,5 +5,5 @@ func ExampleSolver558() {
 	Solver554(5)
 	Solver554(6)
 	//Output: a = 3, b = 4, c = 5 ( 9  +  16  =  25 )
-	//a = 3, b = 4, c = 5 ( 9  +  16  =  25
+	//a = 3, b = 4, c = 5 ( 9  +  16  =  25 )
 }
